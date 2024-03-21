6b42215ea1c29306f8a8a4407660c085d7b135c7
Michael322312 <zomisha0808@gmail.com>
2024-03-21 15:57:51 +0200

    Main idea done

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from .forms import CustomCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import CustomUser
 
-# Create your views here.
+@login_required
+def account_info(request):
+    user = CustomUser.objects.get(id=request.user.id)
+
+    return render(
+        request,
+        "auth/user_edit.html",
+        context = {"user": user}
+    )
+
+
+def register(request):
+    if request.method == "POST":
+        form = CustomCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("countries")
+    else:
+        form = CustomCreationForm()
+        context = {
+            "form": form,
+            "request": request
+        }
+
+        return render(
+            request,
+            "auth/register_form.html",
+            context
+        )
+   
+
+def log_in(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("countries")
+            else:
+                messages.error(request, "Password or username is incorrect!")
+
+    else:
+        form = AuthenticationForm()
+
+    return render(
+        request,
+        "auth/login_form.html",
+        context={"form": form}
+    )
+
+
+@login_required(redirect_field_name="/")
+def log_out(request):
+    logout(request)
+    return redirect("countries")
--- booking/urls.py
+++ booking/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', get_countries),
-    path('country_hotels', get_hotels_in_country),
-    path('hotel/<int:hotel_id>', get_hotel_rooms),
-    path('booking/<int:pk>', booking_form),
-    path('booking_details/<int:pk>', booking_details, name="booking_details")
+    path('', get_countries, name="countries"),
+    path('country_hotels', get_hotels_in_country, name="country_hotels"),
+    path('hotel/<int:hotel_id>', get_hotel_rooms, name="hotel_rooms"),
+    path('booking/<int:pk>', booking_form, name="booking_form"),
+    path('booking_details/<int:pk>', booking_details, name="booking_details"),
+    path('user_info', get_user_info, name="user_info"),
+    path('user_bookings', get_user_bookings, name="user_bookings")
 ] 
